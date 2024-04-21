277c8e146f712ec25b1e03db2e999fd3ed937d18
jacqubu <jacqubu@umich.edu>
2024-04-21 15:35:25 -0400

    Removed linear_regression.R from git

--- linear_regression.R
+++ linear_regression.R
@@ -4,6 +4,13 @@ census_api_key("5b2979d9a21fa6439568ee0cc13cf19cb1c52797", install=TRUE)
 
 v22 <- load_variables(year = 2022, dataset = "acs5", cache = TRUE)
 View(v22)
+
+#B09021_002 Estimate!!Total:!! Lives alone
+#B17020_001 Poverty Status in the Past 12 Months by Age
+
+#B18107_001 Estimate!!Total:  Sex by Age by Independent Living Difficulty
+
+
 #then search for Estimate!!Total:!!Income in the past 12 months below poverty level:
 #and find that the variable is B17001_002
 get_acs(geography = "state", variables = c(poverty = "B17001B_002"), state="MI", year = 2022, progress = FALSE)
